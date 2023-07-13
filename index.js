3956530a3b7247f310620d54de842a6e80117bb2
BrookeBryski <brookeabryski@gmail.com>
2023-07-13 12:57:44 -0700

    index and new

--- index.js
+++ index.js
@@ -7,6 +7,40 @@ app.use(express.urlencoded({ extended: true }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+let hobbies = [
+
+    {
+        feeling: 'empowered',
+        activity: 'solo travel'
+    },
+    {
+        feeling: 'zen',
+        activity: 'yoga'
+    },
+    {
+        feeling: 'strong',
+        activity: 'working out'
+    },
+    {
+        feeling: 'present',
+        activity: 'meditating'
+    }  
+]
+
+app.get('/hobbies', (req, res) => {
+    res.render('hobbies/index', { hobbies })
+})
+
+app.get('/hobbies/new', (req, res) => {
+    res.render('hobbies/new')
+})
+
+app.post('/hobbies', (req, res) => {
+    const { feeling, activity } = req.body;
+    hobbies.push({ feeling, activity });
+    res.redirect('/hobbies');
+})
+
 app.listen(3000, () => {
     console.log("listening on port 3000");
 })
