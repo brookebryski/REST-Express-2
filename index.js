93ef8677a29095913c63eef14c4b3501e8938e06
BrookeBryski <brookeabryski@gmail.com>
2023-07-13 14:19:02 -0700

    show route and uuid

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
+
 
 app.use(express.urlencoded({ extended: true }))
 
@@ -10,18 +12,22 @@ app.set('view engine', 'ejs')
 let hobbies = [
 
     {
+        id: uuid(),
         feeling: 'empowered',
         activity: 'solo travel'
     },
     {
+        id: uuid(),
         feeling: 'zen',
         activity: 'yoga'
     },
     {
+        id: uuid(),
         feeling: 'strong',
         activity: 'working out'
     },
     {
+        id: uuid(),
         feeling: 'present',
         activity: 'meditating'
     }  
@@ -37,8 +43,14 @@ app.get('/hobbies/new', (req, res) => {
 
 app.post('/hobbies', (req, res) => {
     const { feeling, activity } = req.body;
-    hobbies.push({ feeling, activity });
+    hobbies.push({ feeling, activity, id: uuid() });
     res.redirect('/hobbies');
+})
+
+app.get('/hobbies/:id', (req, res) => {
+    const { id } = req.params;
+    const foundHobby = hobbies.find(h => h.id === id);
+    res.render('hobbies/show', { foundHobby });
 })
 
 app.listen(3000, () => {
